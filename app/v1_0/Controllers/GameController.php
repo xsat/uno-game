f401b0ff9d6f35a4d0409cb84d73c67231fe6139
Xsat <xsat.php@gmail.com>
2017-06-27 22:10:19 +0300

    Added verify token

--- app/v1_0/Controllers/GameController.php
+++ app/v1_0/Controllers/GameController.php
@@ -19,5 +19,10 @@ class GameController extends BaseController
      */
     public function callbackAction()
     {
+        if ($this->request->get('hub_mode') == 'subscribe' &&
+            $this->request->get('hub_verify_token') == getenv('VERIFY_TOKE')
+        ) {
+            $this->response->setContent($this->request->get('hub_challenge'));
+        }
     }
 }
